e712f286986b4d35b25f8a15ddf1aac79b5cca6a
gruxeon2507 <khieuminhduc2012@gmail.com>
2024-03-11 10:24:31 +0700

    Add database

---
+++ _PRN.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE [PRN221_MealManagement]
+GO
+
+USE [PRN221_MealManagement]
+GO
+
+CREATE TABLE [users] (
+  [id] integer PRIMARY KEY,
+  [username] nvarchar(255),
+  [password] nvarchar(255),
+  [email] nvarchar(255),
+  [role] nvarchar(255),
+)
+GO
+
+CREATE TABLE [recipes] (
+  [id] integer PRIMARY KEY,
+  [name] nvarchar(255)
+)
+GO
+
+CREATE TABLE [ingredients] (
+  [id] integer PRIMARY KEY,
+  [name] integer,
+  [price] integer
+)
+GO
+
+CREATE TABLE [recipes_ingredients] (
+  [id] integer PRIMARY KEY,
+  [recipeId] integer,
+  [ingredientId] integer,
+  [amount] integer
+)
+GO
+
+CREATE TABLE [meals] (
+  [id] integer PRIMARY KEY,
+  [date] datetime,
+  [createdBy] integer
+)
+GO
+
+CREATE TABLE [meals_recipes] (
+  [id] integer PRIMARY KEY,
+  [recipeId] integer,
+  [mealId] integer
+)
+GO
+
+ALTER TABLE [meals] ADD FOREIGN KEY ([createdBy]) REFERENCES [users] ([id])
+GO
+
+ALTER TABLE [meals_recipes] ADD FOREIGN KEY ([mealId]) REFERENCES [meals] ([id])
+GO
+
+ALTER TABLE [meals_recipes] ADD FOREIGN KEY ([recipeId]) REFERENCES [recipes] ([id])
+GO
+
+ALTER TABLE [recipes_ingredients] ADD FOREIGN KEY ([recipeId]) REFERENCES [recipes] ([id])
+GO
+
+ALTER TABLE [recipes_ingredients] ADD FOREIGN KEY ([ingredientId]) REFERENCES [ingredients] ([id])
+GO
