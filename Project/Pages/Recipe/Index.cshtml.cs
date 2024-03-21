65946e4d5cd5d82d1fcdb02d8ba2c9471a835b44
gruxeon2507 <khieuminhduc2012@gmail.com>
2024-03-21 08:56:48 +0700

    add pagination + search bar

--- Project/Pages/Recipe/Index.cshtml.cs
+++ Project/Pages/Recipe/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using Project.Models;
 
 namespace Project.Pages.Recipe
@@ -19,13 +20,37 @@ namespace Project.Pages.Recipe
         }
 
         public IList<Models.Recipe> Recipe { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public int PageSize { get; private set; } = 10; // Number of items per page
+        public int TotalPages { get; private set; }
+        public int CurrentPage { get; private set; } = 1;
+        public int Page = 1;
+        public string SearchInput;
+        public async Task OnGetAsync(string searchInput)
         {
-            if (_context.Recipes != null)
+            if(!string.IsNullOrEmpty(searchInput))
             {
-                Recipe = await _context.Recipes.ToListAsync();
+                Recipe = await _context.Recipes.Where(r => r.Id.ToString() == searchInput || r.Name.Contains(searchInput)).ToListAsync();
+                SearchInput=searchInput;
             }
+            else
+            {
+                if (_context.Recipes != null)
+                {
+                    Recipe = await _context.Recipes.ToListAsync();
+                }
+            }
+            if (Request.Query.ContainsKey("page") && int.TryParse(Request.Query["page"], out int page))
+            {
+                Page = page;
+            }
+            else
+            {
+                Page = 1; // Default to page 1 if no page parameter is provided or if it's invalid
+            }
+            CurrentPage = Page == 0 ? 1 : Page;
+            TotalPages = TotalPages = (int)Math.Ceiling(Recipe.Count / (double)PageSize);
+            CurrentPage = CurrentPage < 1 ? 1 : (CurrentPage > TotalPages ? TotalPages : CurrentPage);
+            Recipe = Recipe.Skip((CurrentPage - 1) * PageSize).Take(PageSize).ToList();
         }
     }
 }
