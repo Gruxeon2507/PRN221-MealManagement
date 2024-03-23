97882e4ee6bf625bf5b5c1ac73da2bf2920b12e3
gruxeon2507 <khieuminhduc2012@gmail.com>
2024-03-23 11:23:28 +0700

    fiix add ingredient to recipe

--- Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
+++ Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b240bf42d0b54ff18d10890360374a0fe6cdae72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a08dc77c6bfb4346915051cc698521468b2c874")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
